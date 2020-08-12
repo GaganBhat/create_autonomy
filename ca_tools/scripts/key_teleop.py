d1c9caccf5dd817004a4698b7cbaf2352ab5d820
Gagan Bhat <gaganbhat6@gmail.com>
2020-08-12 22:19:37 +0000

    Modified createRules and teleop to fit ease

--- ca_tools/scripts/key_teleop.py
+++ ca_tools/scripts/key_teleop.py
@@ -185,9 +185,9 @@ class SimpleKeyTeleop():
 
         self._hz = rospy.get_param('~hz', 10)
 
-        self._forward_rate = rospy.get_param('~forward_rate', 0.15)
-        self._backward_rate = rospy.get_param('~backward_rate', 0.15)
-        self._rotation_rate = rospy.get_param('~rotation_rate', 0.8)
+        self._forward_rate = rospy.get_param('~forward_rate', 1)
+        self._backward_rate = rospy.get_param('~backward_rate', 1)
+        self._rotation_rate = rospy.get_param('~rotation_rate', 1)
         self._last_pressed = {}
         self._angular = 0
         self._linear = 0
